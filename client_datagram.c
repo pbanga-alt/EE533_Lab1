373d3562be38b70e6ddb7361adbe61564aaff3e8
pbanga-alt <pbanga@usc.edu>
2026-01-16 10:59:55 -0800

    Client side C codes and compiled executable files

---
+++ client_datagram.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n, fromlen;
+
+    struct sockaddr_in serv_addr, from;
+    struct hostent *server;
+
+    char buffer[256];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    printf("Please enter the message: ");
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
+    fromlen = sizeof(struct sockaddr_in); 
+    n = sendto(sockfd,buffer,strlen(buffer),0,(struct sockaddr *) &serv_addr,fromlen);
+    if (n < 0) 
+         error("ERROR writing to socket");
+    bzero(buffer,256);
+    n = recvfrom(sockfd,buffer,255,0,(struct sockaddr *)&serv_addr,&fromlen);
+    if (n < 0) 
+         error("ERROR reading from socket");
+    printf("%s\n",buffer);
+    return 0;
+}
